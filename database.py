1609cd505eca7ab944cd6df56f3299cbf5c68907
Daniele Papperini <daniele.papperini@wearesocial.net>
2024-05-02 12:01:44 +0200

    update database set os.getenv

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = f"postgresql://doadmin:{os.getenv('DB_PASSWORD')}@db-ai-knowledge-base-do-user-14399519-0.b.db.ondigitalocean.com:25060/{os.getenv('DB_NAME')}?sslmode=require"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_ENDPOINT')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB_NAME')}?sslmode=disable"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
