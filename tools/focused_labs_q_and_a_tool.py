a6fbd9f84012d4e7832c09b76cbefb7f6b1ace45
Daniele Papperini <daniele.papperini@wearesocial.net>
2024-05-02 12:26:19 +0200

    update tool Q&A with AmazonKnowledgeBasesRetriever

--- tools/focused_labs_q_and_a_tool.py
+++ tools/focused_labs_q_and_a_tool.py
@@ -1,39 +1,26 @@
 import os
 
-import pinecone
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
-from langchain.chat_models import ChatOpenAI, openai
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
-from config import PINECONE_ENVIRONMENT, PINECONE_INDEX, EMBEDDING_MODEL
+from langchain_community.llms.bedrock import Bedrock
+from langchain_community.retrievers.bedrock import AmazonKnowledgeBasesRetriever
 
 load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-openai.api_key = OPENAI_API_KEY
 
-
-def create_vector_db_tool(llm: ChatOpenAI):
-    pinecone.init(
-        api_key=os.getenv('PINECONE_API_KEY'),
-        environment=PINECONE_ENVIRONMENT
-    )
-    text_field = "text"
-
-    index = pinecone.Index(PINECONE_INDEX)
-
-    embedding_model = OpenAIEmbeddings(
-        model=EMBEDDING_MODEL,
-        openai_api_key=OPENAI_API_KEY
-    )
-
-    vectorstore = Pinecone(
-        index, embedding_model.embed_query, text_field
+def create_vector_db_tool(llm: Bedrock):
+    # Get retriever from vectorstore
+    retriever = AmazonKnowledgeBasesRetriever(
+        knowledge_base_id=os.getenv('AWS_HELLO_KNOWLEDGE_BASE_ID'),
+        retrieval_config={"vectorSearchConfiguration": {"numberOfResults": 3}},
+        # endpoint_url=endpoint_url
+        region_name=os.getenv('AWS_DEFAULT_REGION'),
+        credentials_profile_name=os.getenv('AWS_PROFILE')
     )
     return RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
         return_source_documents=True,
         input_key="question",
-        retriever=vectorstore.as_retriever(search_kwargs={"k": 3})
+        retriever=retriever
     )
